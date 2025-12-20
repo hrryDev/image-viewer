473b06c12b286d7dbdc78b36a32e4079ef30d607
hrryDev <hrry.dev@gmail.com>
2025-12-20 12:01:42 +0000

    image reading done

--- load_img.c
+++ load_img.c
@@ -1,19 +1,20 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
-struct ppm_pixel {
+typedef struct {
 	uint8_t red;
 	uint8_t green;
 	uint8_t blue;
-};
+} ppm_pixel;
 
 FILE* open_file(const char *filename) {
 	FILE* file;
 	file = fopen(filename, "r");
-
+	
 	if (file == NULL) {
 		printf("Error opening file\n");
 		exit(1);
@@ -22,11 +23,36 @@ FILE* open_file(const char *filename) {
 	return file;
 }
 
-void read_ppm(FILE* file, const int width) {
-	char line[width];
+void read_ppm(FILE* file, const int width, const int height) {
+	int line_width, max_value;
+	// 3 (color channels) * 8 (bits/channel) = 24
+	line_width = width * 24;
+	char line[line_width];
 
-	while (fgets(line, sizeof(line), file)) {
-		printf("%s", line);
+	// Read max value TODO: figure out what this would be used for
+	fgets(line, sizeof(line), file);
+	max_value = atoi(line);
+	
+	// Reading pixel data.
+	ppm_pixel pixel_buffer;
+	size_t bytesRead;
+	ppm_pixel image_data[width * height];
+
+	for (int row = 0; row < height; ++row) {
+		for (int pixel = 0; pixel < width; ++pixel) {
+			bytesRead = fread(&pixel_buffer, sizeof(ppm_pixel), 1, file);
+
+			if (bytesRead == 1) {
+				printf("\nPixel %d read.\n", pixel );
+				printf("Red = %d\n", pixel_buffer.red);
+				printf("Green = %d\n", pixel_buffer.green);
+				printf("Blue = %d\n\n", pixel_buffer.blue);
+				image_data[pixel] = pixel_buffer;
+			} else {
+				fprintf(stderr, "Error reading pixel data.\n");
+				exit(1);
+			}
+		}
 	}
 
 	fclose(file);
@@ -38,27 +64,27 @@ int main(const int argc, const char *argv[]) {
 		exit(1);
 	}
 
-	int width;
-	char format[256];
+	int width, height;
+	char line[256];
 	FILE* file;
 	file = open_file(argv[1]);
 
-	// fgets(format, sizeof(format), file);
-	// if (strcmp(format, "P6") != 0) {
-	// 	fprintf(stderr, "Invalid PPM format\n");
-	// 	fclose(file);
-	// 	return 1;
-	// }
-
-	char line[256];
+	// Reading format.
 	fgets(line, sizeof(line), file);
-	while (line[0] == '#') {
+	if (strcmp(line, "P6\n") == 0) {
+		printf("Image is in PPM format\n");
+	}
+
+	// Clearing comment lines.
+	fgets(line, sizeof(line), file);
+	while (line[0] == '#' || line[0] == '\n') {
 		fgets(line, sizeof(line), file);
 	}
 	
-	width = atoi(strtok(line, " "));
+	sscanf(line, "%d %d", &width, &height);
+	printf("width = %d, height = %d\n", width, height);
 
-	read_ppm(file, width);
+	read_ppm(file, width, height);
 
-	return 1;
+	return 0;
 }
